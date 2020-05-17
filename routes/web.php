b2123b048297ddcf84327e8a34785fd275ea7e87
ramkodgreat <nurudeenakindele8@gmail.com>
2020-05-17 10:22:08 +0100

    added welcome route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/us','CountryController@unitedStates')->name('us');
 Route::get('/uk','CountryController@unitedKingdom')->name('uk');
